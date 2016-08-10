72afb1285865383647b85ee851c723b5295c0ad7
Arbab Nazar <arbab.nazar@careaxiom.com>
2016-08-11 00:08:54 +0500

    fix indentation

--- terraform-aws/site.tf
+++ terraform-aws/site.tf
@@ -11,9 +11,9 @@ module "vpc_subnets" {
 	enable_dns_support = true
 	enable_dns_hostnames = true
 	vpc_cidr = "172.16.0.0/16"
-    public_subnets_cidr = "172.16.10.0/24,172.16.20.0/24"
-    private_subnets_cidr = "172.16.30.0/24,172.16.40.0/24"
-    azs    = "eu-west-1a,eu-west-1b"
+        public_subnets_cidr = "172.16.10.0/24,172.16.20.0/24"
+        private_subnets_cidr = "172.16.30.0/24,172.16.40.0/24"
+        azs    = "eu-west-1a,eu-west-1b"
 }
 
 module "ssh_sg" {
